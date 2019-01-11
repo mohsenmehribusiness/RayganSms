5beeefcd61a0e29fe9bb0ab2fea18f7d4f1da56c
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2019-01-11 14:41:45 +0100

    Scrutinizer Auto-Fixes (#5)
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Laravel/RayganSmsServiceProvider.php
+++ src/Laravel/RayganSmsServiceProvider.php
@@ -8,7 +8,7 @@ class RayganSmsServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton('RayganSms', function ($app) {
+        $this->app->singleton('RayganSms', function($app) {
             $conf = $app['config']['services.raygansms'];
 
             return new Sms($conf['user_name'], $conf['password'], $conf['phone_number']);
