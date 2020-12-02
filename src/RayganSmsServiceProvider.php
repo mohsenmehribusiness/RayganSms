00e3d1531a9f32a87debeb95988ff1e43acacca4
mohsen mehri <70861883+mohsenmehribusiness@users.noreply.github.com>
2020-12-03 03:29:00 +0330

    Update RayganSmsServiceProvider.php
    solve problem get information an services.php =>raygansms

--- src/RayganSmsServiceProvider.php
+++ src/RayganSmsServiceProvider.php
@@ -9,7 +9,7 @@ class RayganSmsServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton('RayganSms', function ($app) {
-            $conf = $app['config']['services.raygansms'];
+            $conf = $app['config']['services']['raygansms'];
 
             return new Sms($conf['user_name'], $conf['password'], $conf['phone_number']);
         });
